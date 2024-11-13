b037ad1ad36c30c00007168abc8c264ffbdec6bd
Sonaton Kundu <91369056+Sonaton43@users.noreply.github.com>
2024-11-13 19:18:56 +0600

    .........

--- js/script.js
+++ js/script.js
@@ -35,3 +35,11 @@ window.onload = function() {
       loop: true
   });
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+    const circles = document.querySelectorAll(".skill-circle");
+    circles.forEach(circle => {
+        const percentage = circle.getAttribute("data-percentage");
+        circle.style.setProperty("--percentage", `${(percentage / 100) * 360}deg`);
+    });
+});
